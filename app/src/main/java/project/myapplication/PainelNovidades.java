52fcf39ece50513a7a89d9be0a98484bfc65665a
fernando1910 <fernando.carmo20@gmail.com>
2015-10-26 21:23:32 -0200

    ajuste click painel principal

--- app/src/main/java/project/myapplication/PainelNovidades.java
+++ app/src/main/java/project/myapplication/PainelNovidades.java
@@ -9,27 +9,40 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 
-public class PainelNovidades extends Fragment {
+public class PainelNovidades extends Fragment implements View.OnClickListener {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_painel_novidades, container, false);
-        LinearLayout llComentario = (LinearLayout) view.findViewById(R.id.llComentario);
-        llComentario.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(),PainelEventosPadrao.class);
-                intent.putExtra("fg_comentario", 1);
-                getActivity().finish();
-                startActivity(intent);
-
-            }
-        });
+        LinearLayout llAlteracoes = (LinearLayout) view.findViewById(R.id.llAlteracoes);
+        LinearLayout llComentarios = (LinearLayout) view.findViewById(R.id.llComentarios);
+        LinearLayout llConvites = (LinearLayout) view.findViewById(R.id.llConvites);
+        llComentarios.setOnClickListener(this);
+        llAlteracoes.setOnClickListener(this);
+        llConvites.setOnClickListener(this);
 
         return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getContext(),PainelEventosPadrao.class);
+        switch (v.getId()){
+            case R.id.llComentarios:
+                intent.putExtra("fg_comentario", 1);
+                break;
+            case R.id.llAlteracoes:
+                intent.putExtra("fg_comentario", 1);
+                break;
+            case R.id.llConvites:
+                intent.putExtra("fg_convite", 1);
+                break;
+        }
+
+        getActivity().finish();
+        startActivity(intent);
+    }
 }
